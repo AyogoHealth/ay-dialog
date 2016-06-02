acc2ffae9c90fb3e3603bf978c2aca9879d9409e
Darryl Pogue <darryl@ayogo.com>
2016-06-02 11:41:54 -0700

    Set it up as an actual node module.

--- dialog.js
+++ dialog.js
@@ -1,8 +1,27 @@
-/*! Copyright 2016 Ayogo Health Inc. */
+/*! Copyright (c) 2016 Ayogo Health Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
 
-angular.module('ngDialog', ['ngAnimate'])
+angular.module('ayDialog', [])
 
-.directive('dialog', function($window) {
+.directive('dialog', ['$window', function($window) {
 //// DIALOG MANAGER //////////////////////////////////////////////////////////
     var dialogStack = [];
     var backdropStack = [];
@@ -619,4 +638,4 @@ angular.module('ngDialog', ['ngAnimate'])
             el.open = el.open;
         }
     };
-});
+}]);
